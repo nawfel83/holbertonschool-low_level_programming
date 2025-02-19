8155be209de4b404057a7b03cf737b862f1a3e8b
nawfel83 <laklitnawfel1@gmail.com>
2025-02-19 22:50:01 +0000

    8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char c;
+	int n;
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	for (n = 48; n <= 22; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
