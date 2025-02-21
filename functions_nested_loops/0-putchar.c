05b81d823c13c14aa533f5087f8c0e424fa43b8f
nawfel83 <laklitnawfel1@gmail.com>
2025-02-21 14:59:57 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-
+#include<stdio.h>
+#include <unistd.h>
 /**
- * main - Prints "Holberton" followed by a new line.
+ * main - Puts "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	write(1 "_putchar\n", 9);
+	write(1, "_putchar\n", 9);
 
 	return (0);
 }
