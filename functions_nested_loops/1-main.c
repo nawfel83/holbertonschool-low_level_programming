8721e4a200d2a140ccca545fdec8652f82e08041
nawfel83 <laklitnawfel1@gmail.com>
2025-02-22 17:00:33 +0000

    1

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Programme principal pour tester print_alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
