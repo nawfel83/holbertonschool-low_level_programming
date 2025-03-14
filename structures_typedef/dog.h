254c92ccd8c002f5054392f9525c1538170f8f2f
nawfel83 <laklitnawfel1@gmail.com>
2025-03-14 13:25:15 +0000

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - Structure for a dog
+* @name: Pointer to a string for dog's name
+* @age: Float value for dog's age
+* @owner: Pointer to a string for owner's name
+* Description: Defines a new struct to store information about a dog,
+* including its name, age, and owner.
+*/
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* DOG_H */
