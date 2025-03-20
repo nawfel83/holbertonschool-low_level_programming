a0b9a592d09fd526e44a7908d0b53f260886fb57
nawfel83 <laklitnawfel1@gmail.com>
2025-03-20 09:51:02 +0000

    op

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,61 +1,74 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
- * op_add - function that adds two integers
- * @a: integer 1
- * @b: integer 2
- *
- * Return: the result of addition
+ * op_add - Retourne la somme de a et b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: La somme de a et b
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - function that substractes two integers
- * @a: integer 1
- * @b: integer 2
- *
- * Return: the result of substracte
+ * op_sub - Retourne la différence de a et b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: La différence de a et b
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - function that multiplies two integers
- * @a: integer 1
- * @b: integer 2
- *
- * Return: the result of multiply
+ * op_mul - Retourne le produit de a et b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: Le produit de a et b
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - function that divides two integers
- * @a: integer 1
- * @b: integer 2
- *
- * Return: the result of division
+ * op_div - Retourne le résultat de la division de a par b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: Le résultat de la division de a par b
  */
+
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - function that return the rest of two integers
- * @a: integer 1
- * @b: integer 2
- *
- * Return: the result of the rest of the division
+ * op_mod -  Retourne le reste de la division de a par b
+ * @a: Le premier opérande
+ * @b: Le second opérande
+ * Return: Le reste de la division de a par b
  */
+
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
