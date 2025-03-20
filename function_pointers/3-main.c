5fcd8b92f176aa495e7caf11dbb5dc75bffe33a1
nawfel83 <laklitnawfel1@gmail.com>
2025-03-20 09:42:14 +0000

    Write a program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,17 +3,16 @@
 #include <stdlib.h>
 
 /**
- * main - Main program
+ * main - Entry point for calculator program
  * @argc: Number of arguments
  * @argv: Array of arguments
- * Return: 0 if successful, an error code otherwise
+ *
+ * Return: 0 on success
  */
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int (*op_func)(int, int);
 	int num1, num2;
-	int result;
-	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
@@ -23,16 +22,13 @@ int main(int argc, char **argv)
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-
-	operator = get_op_func(argv[2]);
-	if (operator == NULL)
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	result = operator(num1, num2);
-	printf("%d\n", result);
-
+	printf("%d\n", op_func(num1, num2));
 	return (0);
+
 }
