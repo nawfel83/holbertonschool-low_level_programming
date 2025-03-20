8223f10b97ec9d7a76a34152df1f74afe851f290
nawfel83 <laklitnawfel1@gmail.com>
2025-03-20 09:45:37 +0000

    Write a program that performs simple operations

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,9 +21,10 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (*s == *(ops[i].op) && *(s + 1) == '\0')
 			return (ops[i].f);
 		i++;
 	}
+
 	return (NULL);
 }
