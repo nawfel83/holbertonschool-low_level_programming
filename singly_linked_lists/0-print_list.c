4117b2c1e06bb307f368b3528dfb416df0925f2c
nawfel <laklitnawfel1@gmail.com>
2025-04-02 11:05:55 +0200

    Add singly linked list implementation with print function

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the linked list
+ *
+ * Return: the number of nodes in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+		count++;
+	}
+	return (count);
+}
